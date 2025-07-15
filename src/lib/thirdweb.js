89636b1dc7cce1f9a0f9379869fc59cba58b782f
masolites <mfmmpcs@gmail.com>
2025-07-15 12:36:54 +0100

    Update thirdweb.js

--- src/lib/thirdweb.js
+++ src/lib/thirdweb.js
@@ -5,8 +5,3 @@ export async function createPlatformWallet(email) {
   const wallet = sdk.wallet.generate();
   return wallet.address;
 }
-
-export async function sendMazolTokens(to, amount) {
-  const contract = await sdk.getContract(process.env.MAZOL_CONTRACT_ADDRESS);
-  await contract.erc20.transfer(to, amount);
-}
