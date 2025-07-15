09c732abf08b617b582f4fac9bb3b6026e52cf0e
masolites <mfmmpcs@gmail.com>
2025-07-15 12:35:47 +0100

    Update user.js

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,4 +1,4 @@
- import { connectToDatabase } from '../../lib/mongodb';
+import { connectToDatabase } from '../../lib/mongodb';
 import { createPlatformWallet } from '../../lib/thirdweb';
 import bcrypt from 'bcryptjs';
 
