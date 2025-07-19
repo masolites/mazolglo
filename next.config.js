281a1ec79393336e6258be782ac0c5818bd62bce
masolites <mfmmpcs@gmail.com>
2025-07-19 13:44:36 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["checkout.flutterwave.com"],
+  },
+  experimental: {
+    esmExternals: 'loose',
+    serverComponentsExternalPackages: ['@thirdweb-dev/sdk'],
+  },
+  env: {
+    NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
+    NEXT_PUBLIC_FLUTTERWAVE_PUBLIC_KEY: process.env.NEXT_PUBLIC_FLUTTERWAVE_PUBLIC_KEY,
+  },
+};
+
+module.exports = nextConfig;
