dee4aefc77a898081b395c9af0295d3d7b3e745a
masolites <mfmmpcs@gmail.com>
2025-07-20 03:04:10 +0100

    Delete src directory

--- src/lib/mongodb.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from "mongodb";
-const uri = process.env.MONGODB_URI;
-let client;
-export async function connectToDatabase() {
-  if (!client) client = new MongoClient(uri);
-  if (!client.topology?.isConnected()) await client.connect();
-  const db = client.db();
-  return { db };
-}
